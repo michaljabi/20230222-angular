655f77b4d74790f6b4d2b95c591fb08867cb5385
Jabi <michal.jabi@gmail.com>
2023-02-22 13:51:16 +0100

    explain(HeaderComponent): use PROPS - Input value to child Component.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component/*, ViewEncapsulation*/ } from '@angular/core';
+import { Component, Input/*, ViewEncapsulation*/ } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,5 @@ import { Component/*, ViewEncapsulation*/ } from '@angular/core';
   // encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent {
-    title = 'Portal Aukcyjny'
+    @Input() myTitle = 'Portal Aukcyjny'
 }

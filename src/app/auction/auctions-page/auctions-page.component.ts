fd1433a11fb0434f8f8097f4baf145807f3d5062
Jabi <michal.jabi@gmail.com>
2023-02-23 16:12:20 +0100

    example: async pipe to handle Observables directly on the view.

--- src/app/auction/auctions-page/auctions-page.component.ts
+++ src/app/auction/auctions-page/auctions-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs'
 import { AuctionItem } from '../auction-item'
 import { AuctionsService } from '../auctions.service'
 
@@ -6,6 +7,7 @@ import { AuctionsService } from '../auctions.service'
   // selector: 'app-auctions-page',
   template: `
     <div class="row">
+      <div> Masz już {{ count | async }} aukcji</div>
       <div class="col-12 col-sm-6 col-md-4 col-lg-3" *ngFor="let item of sampleAuctions">
         <app-auction-card [auction]="item"></app-auction-card>
       </div>
@@ -17,9 +19,12 @@ export class AuctionsPageComponent implements OnInit {
   // Zahardkodowane przykładowe dane AuctionItem:
   sampleAuctions: AuctionItem[] = []
 
+  count: Observable<number> = interval(1000)
+
   constructor(private auctionsService: AuctionsService) {}
 
   ngOnInit(): void {
+    // tu jest pewien problem, którego nie ma | async
     this.auctionsService.getAllAuctions().subscribe((auctions) => {
         this.sampleAuctions = auctions
     });

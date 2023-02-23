66b55ddf5a7534d692c7ad445290169bcda97368
Jabi <michal.jabi@gmail.com>
2023-02-23 10:16:06 +0100

    feat(auctions-page): new component connected with "auction" feature.

--- src/app/auction/auction.module.ts
+++ src/app/auction/auction.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuctionRoutingModule } from './auction-routing.module';
+import { AuctionsPageComponent } from './auctions-page/auctions-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuctionsPageComponent
+  ],
   imports: [
     CommonModule,
     AuctionRoutingModule

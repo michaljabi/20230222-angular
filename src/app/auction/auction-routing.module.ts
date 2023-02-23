c5232ca46c89306c573a63f24797928b25768042
Jabi <michal.jabi@gmail.com>
2023-02-23 10:29:08 +0100

    feat(auction-routing): segment 'auctions' route to AuctionsPageComponent

--- src/app/auction/auction-routing.module.ts
+++ src/app/auction/auction-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuctionsPageComponent } from './auctions-page/auctions-page.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auctions', component: AuctionsPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

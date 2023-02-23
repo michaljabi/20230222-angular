49c3df8177d5715d3138a28c5e7ebd1d82008605
Jabi <michal.jabi@gmail.com>
2023-02-23 09:58:46 +0100

    feat(auction module): new module to gather all things connected with auction.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { AuctionModule } from './auction/auction.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MainMenuComponent } from './main-menu/main-menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuctionModule
   ],
   providers: [],
   bootstrap: [AppComponent]

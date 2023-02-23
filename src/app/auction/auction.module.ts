ccd8be3c7e88dcbd1d6dc4e4dd2a300ccf23a38a
Jabi <michal.jabi@gmail.com>
2023-02-23 12:25:58 +0100

    feat(AuctionCardComponent): show sample auctions.

--- src/app/auction/auction.module.ts
+++ src/app/auction/auction.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AuctionRoutingModule } from './auction-routing.module';
 import { AuctionsPageComponent } from './auctions-page/auctions-page.component';
 import { PromotionsPageComponent } from './promotions-page/promotions-page.component';
+import { AuctionCardComponent } from './auction-card/auction-card.component';
 
 
 @NgModule({
   declarations: [
     AuctionsPageComponent,
-    PromotionsPageComponent
+    PromotionsPageComponent,
+    AuctionCardComponent
   ],
   imports: [
     CommonModule,
--- src/app/auction/auctions-page/auctions-page.component.ts
+++ src/app/auction/auctions-page/auctions-page.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
+import { AuctionItem } from '../auction-item'
 
 @Component({
   // selector: 'app-auctions-page',
   template: `
-    <p>
-      auctions-page works!
-    </p>
+    <div class="row">
+      <div class="col-12 col-sm-6 col-md-4 col-lg-3" *ngFor="let item of sampleAuctions">
+        <app-auction-card [auction]="item"></app-auction-card>
+      </div>
+    </div>
   `,
-  styles: [
-  ]
+  styles: []
 })
 export class AuctionsPageComponent {
-
+  // Zahardkodowane przykładowe dane AuctionItem:
+  sampleAuctions: AuctionItem[] = [
+    {
+      id: 2231,
+      imgUrl: 'https://picsum.photos/id/36/600/600',
+      price: 2000,
+      title: 'Części do aparatu'
+    },
+    {
+      description: 'Używany - ale sprawny',
+      id: 2,
+      imgUrl: 'https://picsum.photos/id/48/200/200',
+      price: 4000,
+      title: 'Mac Book'
+    },
+  ]
 }

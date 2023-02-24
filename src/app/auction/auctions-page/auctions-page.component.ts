ac0e64e77d9b6923dbb04d63c0f00c867f5d82c5
Jabi <michal.jabi@gmail.com>
2023-02-24 10:37:27 +0100

    solved(day-3): part 3

--- src/app/auction/auctions-page/auctions-page.component.ts
+++ src/app/auction/auctions-page/auctions-page.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http'
 import { Component, OnInit } from '@angular/core';
 import { interval, Observable } from 'rxjs'
 import { AuctionItem } from '../auction-item'
@@ -8,6 +9,12 @@ import { AuctionsService } from '../auctions.service'
   template: `
     <div class="row">
       <div> Masz już {{ count | async }} aukcji</div>
+      <div class="col-12" *ngIf="errorMessage">
+        <div class="alert alert-danger"> {{ errorMessage }} 😭 ! </div>
+      </div>
+      <div class="col-12" *ngIf="areAuctionsLoading">
+        <div class="alert alert-info"> Poczekaj... ładuję aukcje... </div>
+      </div>
       <div class="col-12 col-sm-6 col-md-4 col-lg-3" *ngFor="let item of sampleAuctions">
         <app-auction-card [auction]="item"></app-auction-card>
       </div>
@@ -20,13 +27,23 @@ export class AuctionsPageComponent implements OnInit {
   sampleAuctions: AuctionItem[] = []
 
   count: Observable<number> = interval(1000)
+  areAuctionsLoading = false;
+  errorMessage = '';
 
   constructor(private auctionsService: AuctionsService) {}
 
   ngOnInit(): void {
+    this.areAuctionsLoading = true;
     // tu jest pewien problem, którego nie ma | async
-    this.auctionsService.getAllAuctions().subscribe((auctions) => {
+    this.auctionsService.getAllAuctions().subscribe({
+      next: (auctions) => {
         this.sampleAuctions = auctions
+        this.areAuctionsLoading = false;
+      },
+      error: (err: HttpErrorResponse) => {
+        this.errorMessage = err.message
+        this.areAuctionsLoading = false;
+      }
     });
   }
 
